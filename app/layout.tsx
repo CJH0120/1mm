103f5f0a925dee3fa428b9d2c8f382981d591bf0
최종현 <choejonghyeon@choejonghyeon-ui-noteubug.local>
2024-05-30 17:29:38 +0900

    GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,12 @@ import AuthContext from "@/utils/AuthContext"
 import image from "@/public/logo/og.png"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
-import Head from "next/head"
+
+import { GoogleAnalytics } from "@next/third-parties/google"
 
 export const metadata: Metadata = {
 	title: {
-		template: "%s - Blog",
+		template: "%s - 1MM",
 		default: "1MM",
 	},
 	metadataBase: new URL("https://1mm.creation.im"),
@@ -19,7 +20,6 @@ export const metadata: Metadata = {
 		url: "https://1mm.creation.im",
 		type: "article",
 		siteName: "1MM",
-
 		title: "1MM",
 		description:
 			"1mm은 일상생활에서 필요한 제품을 가성비,리뷰로 비교하며 여러분에게 더욱 유익한 정보를 전달해 드립니다.  ",
@@ -38,6 +38,7 @@ export default function RootLayout({
 					content="c234979fafb9c7228f99f0d62b28e87a3ac7ba64"
 				/>
 			</head>
+
 			<body>
 				<SpeedInsights />
 				<Analytics />
@@ -45,6 +46,7 @@ export default function RootLayout({
 					<main className="min-h-screen flex flex-col">{children}</main>
 				</AuthContext>
 			</body>
+			<GoogleAnalytics gaId="G-348XQPBTET" />
 		</html>
 	)
 }

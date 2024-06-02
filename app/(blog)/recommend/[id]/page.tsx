b6b4c7bc1d44d11821caf19b43cd1d043b89fff9
최종현 <jonghyun@choejonghyeon-ui-Macmini.local>
2024-06-02 19:59:46 +0900

    [2024/06/02] 에디터 추가

--- app/(blog)/recommend/[id]/page.tsx
+++ app/(blog)/recommend/[id]/page.tsx
@@ -1,21 +1,21 @@
 import Image from "next/image"
 import { prisma } from "@/db/prisma"
 import { notFound } from "next/navigation"
-import { Card, CardDescription, CardTitle } from "@/components/ui/card"
-import Link from "next/link"
-import { Button } from "@/components/ui/button"
 import { Metadata, ResolvingMetadata } from "next"
-import { UserSelectLog } from "../../_action/useUserSelect"
 import Quickly from "../../_components/quickly"
 import ProductSection from "../../_components/productSection"
 
 export const dynamic = "force-static"
+export const revalidate = false
+
 import dynamicImport from "next/dynamic"
 
 const DynamicPreview = dynamicImport(
 	() => import("@/components/comm/Preview"),
+
 	{
-		loading: () => <p>Loading...</p>,
+		ssr: false,
+		loading: () => null,
 	}
 )
 interface DetailProps {
@@ -97,7 +97,7 @@ const RecommendDetail = async ({ params }: { params: { id: string } }) => {
 	return (
 		<div className="px-5 w-full max-w-[777px]">
 			<article className="h-fit">
-				<header className="w-full pt-[24px]">
+				<header className="w-full p-[24px]">
 					<div className="w-full  relative aspect-square max-h-[300px]  rounded-sm	 ">
 						<Image
 							style={{ objectFit: "cover" }}
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	images: {
+		deviceSizes: [120, 500],
+	},
 	experimental: {
+		webVitalsAttribution: ["CLS", "LCP"],
+		optimizePackageImports: [
+			"@tiptap/pm",
+			"@tiptap/react",
+			"@tiptap/starter-kit",
+			"lucide-react",
+		],
 		serverActions: {
 			bodySizeLimit: "2mb",
 		},

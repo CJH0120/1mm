5718d2a1787bcd11740f28a5da7ae84949cdddf9
최종현 <jonghyun@choejonghyeon-ui-Macmini.local>
2024-06-03 21:02:50 +0900

    [2024/06/03]

---
+++ app/auth/ddd/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useEffect } from "react"
+import { useRouter } from "next/navigation"
+const DDD = () => {
+	const router = useRouter()
+	useEffect(() => {
+		router.push("https://link.coupang.com/a/bD9U2H")
+	}, [router])
+	return null
+}
+export default DDD
